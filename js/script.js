620fbe8c018df41688150433f2a9cfff66db08b9
Chicco80 <chicco80lisa@gmail.com>
2022-03-29 17:01:20 +0200

    ex-1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 
 
+  const users = ['user1@gmail.com', 'user2@gmail.com', 'chicco@gmail.com'];
+
+  let ok = false;
+
+  document.getElementById("submit").addEventListener("clic", okMail);
+
+  function okMail(){
+
+    let userMail = document.getElementById("email").value;
+   for(let i=0; i< users.length; i++){
+      let emailVera = users[i];
+      if (userMail === emailVera){
+        ok = true;
+      }
+      console.log(ok,userMail,emailVera)
+   }
+    if(ok){
+      alert("benvenuto");
+   }else{
+      alert("mail non valida")
+    }
+  }
 
 
 
     
-
