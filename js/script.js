141029f861c1c8096b6cf7903c5a03e60f53d0a4
Chicco80 <chicco80lisa@gmail.com>
2022-03-29 18:16:32 +0200

    esercizio 2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
-
+// creo array
   const users = ['user@gmail.com', 'userabac@gmail.com', 'chicco@gmail.com'];
-
+//controllo
   let ok = false;
-
+//bottone
   document.getElementById("submit").addEventListener("click", okMail);
 
   function okMail(){
@@ -22,6 +22,27 @@
       alert("mail non valida")
     }
   }
+
+
+  //gioco dadi
+
+  document.getElementById("tira").addEventListener("click", tiraDado);
+
+  function tiraDado(){
+      
+         let giocatore = Math.floor(Math.random() * 6) +1;
+         let giocatore1= Math.floor(Math.random() * 6) +1;
+
+
+         document.getElementById("ris").innerHTML =`<p>giocatore 1: ${giocatore}</p> <p>giocatore 2: ${giocatore1}</p>`;
+
+         console.log(giocatore);
+         if(giocatore<giocatore1)
+         document.innerHTML=
+
+     }
+     
+  
 
 
 
